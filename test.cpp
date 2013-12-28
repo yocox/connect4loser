bef67cad000fbf7de16299cd631d4ec8877e6296
user <yoco@ubuntu.(none)>
2013-12-29 00:45:07 +0800

    add StringCounter

--- test.cpp
+++ test.cpp
@@ -83,41 +83,6 @@ struct Table
 };
 
 /////////////////////////////////////////////////////////////////////////////
-// Compile Time Unroll Pattern Matcher
-/////////////////////////////////////////////////////////////////////////////
-
-template <int XI, int YI, int D, Color ... COLORS>
-struct Pattern;
-
-template <int XI, int YI, int D, Color Head, Color ... Tail>
-struct Pattern<XI, YI, D, Head, Tail...> {
-    bool static check(const Table& t, const int x, const int y) {
-        return t(x + D * XI, y + D * YI) == Head && Pattern<XI, YI, D + 1, Tail...>::check(t, x, y);
-    }
-};
-
-template <int XI, int YI, int D>
-struct Pattern<XI, YI, D> {
-    bool static check(const Table& t, const int x, const int y) {
-        return true;
-    }
-};
-
-template <int XI, int YI, int D, Color ... COLORS>
-struct SymPattern {
-    bool static check(const Table& t, const int x, const int y) {
-        return Pattern<XI, YI,  D, COLORS...>::check(t, x, y) ||
-               Pattern<-XI, -YI, D, COLORS...>::check(t, x + XI * (static_cast<int>(sizeof...(COLORS)-1)), y + YI * (static_cast<int>(sizeof...(COLORS)) - 1));
-    }
-};
-
-template <Color ... Colors> using HoriPattern = SymPattern<1, 0, 0, Colors...>;
-template <Color ... Colors> using VertPattern = SymPattern<0, 1, 0, Colors...>;
-template <Color ... Colors> using Dia1Pattern = SymPattern<1, 1, 0, Colors...>;
-template <Color ... Colors> using Dia2Pattern = SymPattern<1,-1, 0, Colors...>;
-
-
-/////////////////////////////////////////////////////////////////////////////
 // ColorString
 /////////////////////////////////////////////////////////////////////////////
 
@@ -188,8 +153,8 @@ struct RepStr {
 };
 
 template <Color C>
-struct RepStr<C, 1> {
-    using type = String<C>;
+struct RepStr<C, 0> {
+    using type = String<>;
 };
 
 // WN<4, 2> = "WWWWNN"
@@ -205,56 +170,55 @@ struct WN {
 // Meta Pattern
 /////////////////////////////////////////////////////////////////////////////
 
-using WWWW = WN<4, 0>;
-using WWWN = WN<3, 1>;
-using WWNN = WN<2, 2>;
-using WNNN = WN<1, 3>;
+using WWWW = WN<4, 0>::type;
+using WWWN = WN<3, 1>::type;
+using WWNN = WN<2, 2>::type;
+using WNNN = WN<1, 3>::type;
 
-
+using BBBB = InvString<WWWW>::type;
+using BBBN = InvString<WWWN>::type;
+using BBNN = InvString<WWNN>::type;
+using BNNN = InvString<WNNN>::type;
 
 /////////////////////////////////////////////////////////////////////////////
 // matcher
 /////////////////////////////////////////////////////////////////////////////
 
-template <int X0, int Y0, int XI, int YI, Color ... COLORS>
+template <int X0, int Y0, int XI, int YI, typename StringType>
 struct Matcher;
 
 template <int X0, int Y0, int XI, int YI, Color Head, Color ... Tail>
-struct Matcher<X0, Y0, XI, YI, Head, Tail...> {
+struct Matcher<X0, Y0, XI, YI, String<Head, Tail...>> {
     enum { len = sizeof...(Tail) + 1 };
-    enum { X0 = X0 };
-    enum { Y0 = Y0 };
-    enum { XI = XI };
-    enum { YI = YI };
+    enum { x0 = X0 };
+    enum { y0 = Y0 };
+    enum { xi = XI };
+    enum { yi = YI };
     bool static check(const Table& t) {
-        return t(X0, Y0) == Head && Matcher<X0 + XI, Y0 + YI, XI, YI, Tail...>::check(t);
+        return t(X0, Y0) == Head && Matcher<X0 + XI, Y0 + YI, XI, YI, String<Tail...>>::check(t);
     }
+    using string_type = String<Head, Tail...>;
 };
 
 template <int X0, int Y0, int XI, int YI>
-struct Matcher<X0, Y0, XI, YI> {
+struct Matcher<X0, Y0, XI, YI, String<>> {
     enum { len = 0 };
-    enum { X0 = X0 };
-    enum { Y0 = Y0 };
-    enum { XI = XI };
-    enum { YI = YI };
-    bool static check(const Table& t) {
+    enum { x0 = X0 };
+    enum { y0 = Y0 };
+    enum { xi = XI };
+    enum { yi = YI };
+    bool static check(const Table&) {
         return true;
     }
     using string_type = String<>;
 };
 
-template <template<int,int> class MatcherType>
-struct MatchCounter {
-    enum { len_pat = MatcherType::len };
-    enum { X0 = 0 };
-    enum { Y0 = 0 };
-    enum { X1 = 3 };
-    enum { Y1 = 3 };
-    static int count(const Table& t) {
-        return MatchCounter_impl<MatcherType, X0, Y0, X0, Y0, X1, Y1>::count(t);
-    }
-};
+template <int XI, int Len> struct XRanger { enum { begin = 0 }; enum { end = W - Len }; };
+template <int Len> struct XRanger<0, Len> { enum { begin = 0 }; enum { end = W }; };
+template <int Len> struct XRanger<-1, Len> { enum { begin = Len - 1 }; enum { end = W }; };
+template <int YI, int Len> struct YRanger { enum { begin = 0 }; enum { end = H - Len }; };
+template <int Len> struct YRanger<0, Len> { enum { begin = 0 }; enum { end = H }; };
+template <int Len> struct YRanger<-1, Len> { enum { begin = Len - 1 }; enum { end = H }; };
 
 template <template<int, int> class MatcherType, int X, int Y, int X0, int Y0, int X1, int Y1>
 struct MatchCounter_impl {
@@ -279,142 +243,60 @@ struct MatchCounter_impl<MatcherType, X, Y1, X0, Y0, X1, Y1> {
 
 template <template<int, int> class MatcherType, int X0, int Y0, int X1, int Y1>
 struct MatchCounter_impl<MatcherType, X1, Y1, X0, Y0, X1, Y1> {
-    static int count(const Table& t) {
+    static int count(const Table&) {
         return 0;
     }
 };
 
+template <template<int, int> class MatcherType>
+struct MatchCounter {
+    enum { len_pat = MatcherType<0, 0>::len };
+    enum { X0 = XRanger<MatcherType<0, 0>::xi, len_pat>::begin };
+    enum { Y0 = YRanger<MatcherType<0, 0>::yi, len_pat>::begin };
+    enum { X1 = XRanger<MatcherType<0, 0>::xi, len_pat>::end };
+    enum { Y1 = YRanger<MatcherType<0, 0>::yi, len_pat>::end };
+    static int count(const Table& t) {
+        std::cout << typeid(typename MatcherType<0, 0>::string_type).name() << len_pat << "(" << X0 << ", " << Y0 << ") ~ (" << X1 << ", " << Y1 << ")" << std::endl;
+        return MatchCounter_impl<MatcherType, X0, Y0, X0, Y0, X1, Y1>::count(t);
+    }
+};
 
-/////////////////////////////////////////////////////////////////////////////
-// horizontal
-/////////////////////////////////////////////////////////////////////////////
-
-using H_WWWW = HoriPattern<WHITE, WHITE, WHITE, WHITE>;
-using H_BBBB = HoriPattern<BLACK, BLACK, BLACK, BLACK>;
-using H_NWWW = HoriPattern< NONE, WHITE, WHITE, WHITE>;
-using H_NBBB = HoriPattern< NONE, BLACK, BLACK, BLACK>;
-using H_BWWW = HoriPattern<BLACK, WHITE, WHITE, WHITE>;
-using H_WBBB = HoriPattern<WHITE, BLACK, BLACK, BLACK>;
-using H_NWW = HoriPattern< NONE, WHITE, WHITE>;
-using H_NBB = HoriPattern< NONE, BLACK, BLACK>;
-using H_BWW = HoriPattern<BLACK, WHITE, WHITE>;
-using H_WBB = HoriPattern<WHITE, BLACK, BLACK>;
-using H_NW = HoriPattern< NONE, WHITE>;
-using H_NB = HoriPattern< NONE, BLACK>;
-using H_BW = HoriPattern<BLACK, WHITE>;
-
-/////////////////////////////////////////////////////////////////////////////
-// vertical
-/////////////////////////////////////////////////////////////////////////////
-
-using V_WWWW = VertPattern<WHITE, WHITE, WHITE, WHITE>;
-using V_BBBB = VertPattern<BLACK, BLACK, BLACK, BLACK>;
-using V_NWWW = VertPattern< NONE, WHITE, WHITE, WHITE>;
-using V_NBBB = VertPattern< NONE, BLACK, BLACK, BLACK>;
-using V_BWWW = VertPattern<BLACK, WHITE, WHITE, WHITE>;
-using V_WBBB = VertPattern<WHITE, BLACK, BLACK, BLACK>;
-using V_NWW = VertPattern< NONE, WHITE, WHITE>;
-using V_NBB = VertPattern< NONE, BLACK, BLACK>;
-using V_BWW = VertPattern<BLACK, WHITE, WHITE>;
-using V_WBB = VertPattern<WHITE, BLACK, BLACK>;
-using V_NW = VertPattern< NONE, WHITE>;
-using V_NB = VertPattern< NONE, BLACK>;
-using V_BW = VertPattern<BLACK, WHITE>;
-
-/////////////////////////////////////////////////////////////////////////////
-// Dia1
-/////////////////////////////////////////////////////////////////////////////
-
-using D1_WWWW = Dia1Pattern<WHITE, WHITE, WHITE, WHITE>;
-using D1_BBBB = Dia1Pattern<BLACK, BLACK, BLACK, BLACK>;
-using D1_NWWW = Dia1Pattern< NONE, WHITE, WHITE, WHITE>;
-using D1_NBBB = Dia1Pattern< NONE, BLACK, BLACK, BLACK>;
-using D1_BWWW = Dia1Pattern<BLACK, WHITE, WHITE, WHITE>;
-using D1_WBBB = Dia1Pattern<WHITE, BLACK, BLACK, BLACK>;
-using D1_NWW = Dia1Pattern< NONE, WHITE, WHITE>;
-using D1_NBB = Dia1Pattern< NONE, BLACK, BLACK>;
-using D1_BWW = Dia1Pattern<BLACK, WHITE, WHITE>;
-using D1_WBB = Dia1Pattern<WHITE, BLACK, BLACK>;
-using D1_NW = Dia1Pattern< NONE, WHITE>;
-using D1_NB = Dia1Pattern< NONE, BLACK>;
-using D1_BW = Dia1Pattern<BLACK, WHITE>;
-
-/////////////////////////////////////////////////////////////////////////////
-// Dai2
-/////////////////////////////////////////////////////////////////////////////
-
-using D2_WWWW = Dia2Pattern<WHITE, WHITE, WHITE, WHITE>;
-using D2_BBBB = Dia2Pattern<BLACK, BLACK, BLACK, BLACK>;
-using D2_NWWW = Dia2Pattern< NONE, WHITE, WHITE, WHITE>;
-using D2_NBBB = Dia2Pattern< NONE, BLACK, BLACK, BLACK>;
-using D2_BWWW = Dia2Pattern<BLACK, WHITE, WHITE, WHITE>;
-using D2_WBBB = Dia2Pattern<WHITE, BLACK, BLACK, BLACK>;
-using D2_NWW = Dia2Pattern< NONE, WHITE, WHITE>;
-using D2_NBB = Dia2Pattern< NONE, BLACK, BLACK>;
-using D2_BWW = Dia2Pattern<BLACK, WHITE, WHITE>;
-using D2_WBB = Dia2Pattern<WHITE, BLACK, BLACK>;
-using D2_NW = Dia2Pattern< NONE, WHITE>;
-using D2_NB = Dia2Pattern< NONE, BLACK>;
-using D2_BW = Dia2Pattern<BLACK, WHITE>;
+template <typename StringType>
+struct StringCounter {
+    template <int X0, int Y0> using HoStr = Matcher<X0, Y0, 1, 0, StringType>;
+    template <int X0, int Y0> using VeStr = Matcher<X0, Y0, 0, 1, StringType>;
+    template <int X0, int Y0> using D1Str = Matcher<X0, Y0, 1, 1, StringType>;
+    template <int X0, int Y0> using D2Str = Matcher<X0, Y0, 1,-1, StringType>;
+    static int count(const Table& t) {
+        return MatchCounter<HoStr>::count(t) +
+               MatchCounter<VeStr>::count(t) +
+               MatchCounter<D1Str>::count(t) +
+               MatchCounter<D2Str>::count(t);
+    }
+};
 
 /////////////////////////////////////////////////////////////////////////
 // Score Weights
 /////////////////////////////////////////////////////////////////////////
 
 const int S_4C = 1000000;
-const int S_3N =  10000;
-const int S_3B =   2000;
-const int S_2N =   1000;
-const int S_2B =    200;
-const int S_1N =    100;
-const int S_1B =     20;
+const int S_3N =   10000;
+const int S_3B =    2000;
+const int S_2N =    1000;
+const int S_2B =     200;
+const int S_1N =     100;
+const int S_1B =      20;
 
-int eval(const Table& t)
+int eval(const Table&)
 {
     int score = 0;
     ///////////////////////////////////////////////////////////
     // win
     ///////////////////////////////////////////////////////////
-    for (int y = 0; y < H    ; ++y) { for (int x = 0; x < W - 3; ++x) { if ( H_BBBB::check(t, x, y)) { return -S_4C; } if ( H_WWWW::check(t, x, y)) { return  S_4C; } } }
-    for (int y = 0; y < H - 3; ++y) { for (int x = 0; x < W    ; ++x) { if ( V_BBBB::check(t, x, y)) { return -S_4C; } if ( V_WWWW::check(t, x, y)) { return  S_4C; } } }
-    for (int y = 0; y < H - 3; ++y) { for (int x = 0; x < W - 3; ++x) { if (D1_BBBB::check(t, x, y)) { return -S_4C; } if (D1_WWWW::check(t, x, y)) { return  S_4C; } } }
-    for (int y = 3; y < H    ; ++y) { for (int x = 0; x < W - 3; ++x) { if (D2_BBBB::check(t, x, y)) { return -S_4C; } if (D2_WWWW::check(t, x, y)) { return  S_4C; } } }
 
-    // WWWN
-    for (int y = 0; y < H    ; ++y) { for (int x = 0; x < W - 3; ++x) { if ( H_NBBB::check(t, x, y)) { score -= S_3N; } if ( H_NWWW::check(t, x, y)) { score += S_3N; } } }
-    for (int y = 0; y < H - 3; ++y) { for (int x = 0; x < W    ; ++x) { if ( V_NBBB::check(t, x, y)) { score -= S_3N; } if ( V_NWWW::check(t, x, y)) { score += S_3N; } } }
-    for (int y = 0; y < H - 3; ++y) { for (int x = 0; x < W - 3; ++x) { if (D1_NBBB::check(t, x, y)) { score -= S_3N; } if (D1_NWWW::check(t, x, y)) { score += S_3N; } } }
-    for (int y = 3; y < H    ; ++y) { for (int x = 0; x < W - 3; ++x) { if (D2_NBBB::check(t, x, y)) { score -= S_3N; } if (D2_NWWW::check(t, x, y)) { score += S_3N; } } }
-
-    // WWWB
-    for (int y = 0; y < H    ; ++y) { for (int x = 0; x < W - 3; ++x) { if ( H_WBBB::check(t, x, y)) { score -= S_3B; } if ( H_BWWW::check(t, x, y)) { score += S_3B; } } }
-    for (int y = 0; y < H - 3; ++y) { for (int x = 0; x < W    ; ++x) { if ( V_WBBB::check(t, x, y)) { score -= S_3B; } if ( V_BWWW::check(t, x, y)) { score += S_3B; } } }
-    for (int y = 0; y < H - 3; ++y) { for (int x = 0; x < W - 3; ++x) { if (D1_WBBB::check(t, x, y)) { score -= S_3B; } if (D1_BWWW::check(t, x, y)) { score += S_3B; } } }
-    for (int y = 3; y < H    ; ++y) { for (int x = 0; x < W - 3; ++x) { if (D2_WBBB::check(t, x, y)) { score -= S_3B; } if (D2_BWWW::check(t, x, y)) { score += S_3B; } } }
-
-    // WWN
-    for (int y = 0; y < H    ; ++y) { for (int x = 0; x < W - 2; ++x) { if ( H_NBB::check(t, x, y)) { score -= S_2N; } if ( H_NWW::check(t, x, y)) { score += S_2N; } } }
-    for (int y = 0; y < H - 2; ++y) { for (int x = 0; x < W    ; ++x) { if ( V_NBB::check(t, x, y)) { score -= S_2N; } if ( V_NWW::check(t, x, y)) { score += S_2N; } } }
-    for (int y = 0; y < H - 2; ++y) { for (int x = 0; x < W - 2; ++x) { if (D1_NBB::check(t, x, y)) { score -= S_2N; } if (D1_NWW::check(t, x, y)) { score += S_2N; } } }
-    for (int y = 2; y < H    ; ++y) { for (int x = 0; x < W - 2; ++x) { if (D2_NBB::check(t, x, y)) { score -= S_2N; } if (D2_NWW::check(t, x, y)) { score += S_2N; } } }
-
-    // WWB
-    for (int y = 0; y < H    ; ++y) { for (int x = 0; x < W - 2; ++x) { if ( H_WBB::check(t, x, y)) { score -= S_2B; } if ( H_BWW::check(t, x, y)) { score += S_2B; } } }
-    for (int y = 0; y < H - 2; ++y) { for (int x = 0; x < W    ; ++x) { if ( V_WBB::check(t, x, y)) { score -= S_2B; } if ( V_BWW::check(t, x, y)) { score += S_2B; } } }
-    for (int y = 0; y < H - 2; ++y) { for (int x = 0; x < W - 2; ++x) { if (D1_WBB::check(t, x, y)) { score -= S_2B; } if (D1_BWW::check(t, x, y)) { score += S_2B; } } }
-    for (int y = 2; y < H    ; ++y) { for (int x = 0; x < W - 2; ++x) { if (D2_WBB::check(t, x, y)) { score -= S_2B; } if (D2_BWW::check(t, x, y)) { score += S_2B; } } }
-
-    // WN
-    for (int y = 0; y < H    ; ++y) { for (int x = 0; x < W - 1; ++x) { if ( H_NB::check(t, x, y)) { score -= S_1N; } if ( H_NW::check(t, x, y)) { score += S_1N; } } }
-    for (int y = 0; y < H - 1; ++y) { for (int x = 0; x < W    ; ++x) { if ( V_NB::check(t, x, y)) { score -= S_1N; } if ( V_NW::check(t, x, y)) { score += S_1N; } } }
-    for (int y = 0; y < H - 1; ++y) { for (int x = 0; x < W - 1; ++x) { if (D1_NB::check(t, x, y)) { score -= S_1N; } if (D1_NW::check(t, x, y)) { score += S_1N; } } }
-    for (int y = 1; y < H    ; ++y) { for (int x = 0; x < W - 1; ++x) { if (D2_NB::check(t, x, y)) { score -= S_1N; } if (D2_NW::check(t, x, y)) { score += S_1N; } } }
-
-    // WB
-    for (int y = 0; y < H    ; ++y) { for (int x = 0; x < W - 1; ++x) { if ( H_BW::check(t, x, y)) { score -= S_1B; } if ( H_BW::check(t, x, y)) { score += S_1B; } } }
-    for (int y = 0; y < H - 1; ++y) { for (int x = 0; x < W    ; ++x) { if ( V_BW::check(t, x, y)) { score -= S_1B; } if ( V_BW::check(t, x, y)) { score += S_1B; } } }
-    for (int y = 0; y < H - 1; ++y) { for (int x = 0; x < W - 1; ++x) { if (D1_BW::check(t, x, y)) { score -= S_1B; } if (D1_BW::check(t, x, y)) { score += S_1B; } } }
-    for (int y = 1; y < H    ; ++y) { for (int x = 0; x < W - 1; ++x) { if (D2_BW::check(t, x, y)) { score -= S_1B; } if (D2_BW::check(t, x, y)) { score += S_1B; } } }
+    ///////////////////////////////////////////////////////////
+    // not win, calculate score
+    ///////////////////////////////////////////////////////////
 
     return score;
 }
@@ -490,23 +372,30 @@ int choose_a_move(Table& t, int level)
 }
 
 template <int X0, int Y0>
-using ma_hori = Matcher<X0, Y0, 1, 0, WHITE, WHITE, WHITE, WHITE>;
+using ma_hori = Matcher<X0, Y0, 1, 0, String<WHITE, WHITE, WHITE, WHITE>>;
 
 int main()
 {
     Table t;
     using WW = String<WHITE, WHITE, WHITE, WHITE>;
+    t.put(0, BLACK);
+    t.put(1, BLACK);
+    t.put(2, BLACK);
+    t.put(3, BLACK);
     t.put(0, WHITE);
     t.put(1, WHITE);
     t.put(2, WHITE);
     t.put(3, WHITE);
-    using maty = ma_hori<0, 0>;
-    bool ee = maty::check(t);
-    int cnt = MatchCounter<ma_hori>::count(t);
+    t.put(0, WHITE);
+    t.put(0, WHITE);
+    t.put(0, WHITE);
+    t.put(1, WHITE);
+    t.put(2, WHITE);
+    t.put(3, WHITE);
+    std::cout << StringCounter<WW>::count(t) << std::endl;
+    //std::cout << MatchCounter<HoriPat<BLACK, BLACK, BLACK, BLACK>::type>::count(t) << std::endl;
 
     return 0;
-
-   
 
 
 
